62fa9bfc0c8e4d538d27ee1c690e6bcb0f585503
Mateo Bohorquez <132773064+Mate8ba8@users.noreply.github.com>
2023-11-01 14:47:41 -0500

    derecha.py

--- fronted/derecha/derecha.py
+++ fronted/derecha/derecha.py
@@ -1,14 +1,13 @@
 
-from dash import Dash, dash_table, dcc, html, Input, Output, callback
-import pandas as pd
-import plotly.graph_objs as go
-import math
+from dash import dcc, html
 from backend.granulometria import*
 from backend.estilos import*
 from backend.imagenes import*
 from backend.Tablas import*
 from backend.cartaplasticidad import*
 import dash_bootstrap_components as dbc
+
+
 
 
 
