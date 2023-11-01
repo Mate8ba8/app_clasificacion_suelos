5551e7a1abd088ac290da9374d5ec8928acb81c8
Mateo Bohorquez <132773064+Mate8ba8@users.noreply.github.com>
2023-11-01 14:49:40 -0500

    cartaplasticidad.py

--- backend/cartaplasticidad.py
+++ backend/cartaplasticidad.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
 import numpy as np
 import io
 import base64
